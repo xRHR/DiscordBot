09bb7d86fba8e0bef239e0d3985aa75708024196
xrhr <xxrhr@yandex.ru>
2024-08-23 12:30:00 +0800

    dsffsd

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -16,6 +16,11 @@ builder.Services.AddHostedService<DiscordClientHost>();
 // Lavalink
 builder.Services.AddLavalink();
 builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
+builder.Services.ConfigureLavalink(config =>
+{
+    config.BaseAddress = new Uri("https://v4.lavalink.rocks:443/");
+    config.Passphrase = "horizxon.tech";
+});
 
 IHost host = builder.Build();
 
