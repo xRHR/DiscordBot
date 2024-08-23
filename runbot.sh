7fb65a0a949fd93d5641dc704661136e4b849aba
xrhr <xxrhr@yandex.ru>
2024-08-23 13:51:56 +0800

    sda

--- runbot.sh
+++ runbot.sh
@@ -4,5 +4,5 @@ echo ===$ docker rmi discord-bot:0.1
 docker rmi discord-bot:0.1
 echo ===$ docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
 docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
-echo ===$ docker run --rm --name discord-bot -i -e DISCORD_TOKEN=$DISCORD_TOKEN discord-bot:0.1
+echo ===$ docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN discord-bot:0.1
 docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN discord-bot:0.1
