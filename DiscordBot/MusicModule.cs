21fe53fc4f70e5390fc72c6c115d8241f6d5d20e
xrhr <xxrhr@yandex.ru>
2024-08-23 08:24:00 +0800

    sad

---
+++ DiscordBot/MusicModule.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Interactions;
+using global::Lavalink4NET.Players.Vote;
+using global::Lavalink4NET.Players;
+using global::Lavalink4NET.Rest.Entities.Tracks;
+using global::Lavalink4NET;
+using Lavalink4NET.DiscordNet;
+using Lavalink4NET.Players;
+using Lavalink4NET.Players.Vote;
+using Lavalink4NET.Rest.Entities.Tracks;
+
+namespace DiscordBot
+{
+
+    /// <summary>
+    ///     Presents some of the main features of the Lavalink4NET-Library.
+    /// </summary>
+    [RequireContext(ContextType.Guild)]
+    public sealed class MusicModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        private readonly IAudioService _audioService;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MusicModule"/> class.
+        /// </summary>
+        /// <param name="audioService">the audio service</param>
+        /// <exception cref="ArgumentNullException">
+        ///     thrown if the specified <paramref name="audioService"/> is <see langword="null"/>.
+        /// </exception>
+        public MusicModule(IAudioService audioService)
+        {
+            ArgumentNullException.ThrowIfNull(audioService);
+
+            _audioService = audioService;
+        }
+
+        /// <summary>
+        ///     Disconnects from the current voice channel connected to asynchronously.
+        /// </summary>
+        /// <returns>a task that represents the asynchronous operation</returns>
+        [SlashCommand("disconnect", "Disconnects from the current voice channel connected to", runMode: RunMode.Async)]
+        public async Task Disconnect()
+        {
+            var player = await GetPlayerAsync().ConfigureAwait(false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            await player.DisconnectAsync().ConfigureAwait(false);
+            await RespondAsync("Disconnected.").ConfigureAwait(false);
+        }
+
+        /// <summary>
+        ///     Plays music asynchronously.
+        /// </summary>
+        /// <param name="query">the search query</param>
+        /// <returns>a task that represents the asynchronous operation</returns>
+        [SlashCommand("play", description: "Plays music", runMode: RunMode.Async)]
+        public async Task Play(string query)
+        {
+            await DeferAsync().ConfigureAwait(false);
+
+            var player = await GetPlayerAsync(connectToVoiceChannel: true).ConfigureAwait(false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            var track = await _audioService.Tracks
+                .LoadTrackAsync(query, TrackSearchMode.YouTube)
+                .ConfigureAwait(false);
+
+            if (track is null)
+            {
+                await FollowupAsync("😖 No results.").ConfigureAwait(false);
+                return;
+            }
+
+            var position = await player.PlayAsync(track).ConfigureAwait(false);
+
+            if (position is 0)
+            {
+                await FollowupAsync($"🔈 Playing: {track.Uri}").ConfigureAwait(false);
+            }
+            else
+            {
+                await FollowupAsync($"🔈 Added to queue: {track.Uri}").ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        ///     Shows the track position asynchronously.
+        /// </summary>
+        /// <returns>a task that represents the asynchronous operation</returns>
+        [SlashCommand("position", description: "Shows the track position", runMode: RunMode.Async)]
+        public async Task Position()
+        {
+            var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            if (player.CurrentItem is null)
+            {
+                await RespondAsync("Nothing playing!").ConfigureAwait(false);
+                return;
+            }
+
+            await RespondAsync($"Position: {player.Position?.Position} / {player.CurrentTrack.Duration}.").ConfigureAwait(false);
+        }
+
+        /// <summary>
+        ///     Stops the current track asynchronously.
+        /// </summary>
+        /// <returns>a task that represents the asynchronous operation</returns>
+        [SlashCommand("stop", description: "Stops the current track", runMode: RunMode.Async)]
+        public async Task Stop()
+        {
+            var player = await GetPlayerAsync(connectToVoiceChannel: false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            if (player.CurrentItem is null)
+            {
+                await RespondAsync("Nothing playing!").ConfigureAwait(false);
+                return;
+            }
+
+            await player.StopAsync().ConfigureAwait(false);
+            await RespondAsync("Stopped playing.").ConfigureAwait(false);
+        }
+
+        /// <summary>
+        ///     Updates the player volume asynchronously.
+        /// </summary>
+        /// <param name="volume">the volume (1 - 1000)</param>
+        /// <returns>a task that represents the asynchronous operation</returns>
+        [SlashCommand("volume", description: "Sets the player volume (0 - 1000%)", runMode: RunMode.Async)]
+        public async Task Volume(int volume = 100)
+        {
+            if (volume is > 1000 or < 0)
+            {
+                await RespondAsync("Volume out of range: 0% - 1000%!").ConfigureAwait(false);
+                return;
+            }
+
+            var player = await GetPlayerAsync(connectToVoiceChannel: false).ConfigureAwait(false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            await player.SetVolumeAsync(volume / 100f).ConfigureAwait(false);
+            await RespondAsync($"Volume updated: {volume}%").ConfigureAwait(false);
+        }
+
+        [SlashCommand("skip", description: "Skips the current track", runMode: RunMode.Async)]
+        public async Task Skip()
+        {
+            var player = await GetPlayerAsync(connectToVoiceChannel: false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            if (player.CurrentItem is null)
+            {
+                await RespondAsync("Nothing playing!").ConfigureAwait(false);
+                return;
+            }
+
+            await player.SkipAsync().ConfigureAwait(false);
+
+            var track = player.CurrentItem;
+
+            if (track is not null)
+            {
+                await RespondAsync($"Skipped. Now playing: {track.Track!.Uri}").ConfigureAwait(false);
+            }
+            else
+            {
+                await RespondAsync("Skipped. Stopped playing because the queue is now empty.").ConfigureAwait(false);
+            }
+        }
+
+        [SlashCommand("pause", description: "Pauses the player.", runMode: RunMode.Async)]
+        public async Task PauseAsync()
+        {
+            var player = await GetPlayerAsync(connectToVoiceChannel: false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            if (player.State is PlayerState.Paused)
+            {
+                await RespondAsync("Player is already paused.").ConfigureAwait(false);
+                return;
+            }
+
+            await player.PauseAsync().ConfigureAwait(false);
+            await RespondAsync("Paused.").ConfigureAwait(false);
+        }
+
+        [SlashCommand("resume", description: "Resumes the player.", runMode: RunMode.Async)]
+        public async Task ResumeAsync()
+        {
+            var player = await GetPlayerAsync(connectToVoiceChannel: false);
+
+            if (player is null)
+            {
+                return;
+            }
+
+            if (player.State is not PlayerState.Paused)
+            {
+                await RespondAsync("Player is not paused.").ConfigureAwait(false);
+                return;
+            }
+
+            await player.ResumeAsync().ConfigureAwait(false);
+            await RespondAsync("Resumed.").ConfigureAwait(false);
+        }
+
+        /// <summary>
+        ///     Gets the guild player asynchronously.
+        /// </summary>
+        /// <param name="connectToVoiceChannel">
+        ///     a value indicating whether to connect to a voice channel
+        /// </param>
+        /// <returns>
+        ///     a task that represents the asynchronous operation. The task result is the lavalink player.
+        /// </returns>
+        private async ValueTask<VoteLavalinkPlayer?> GetPlayerAsync(bool connectToVoiceChannel = true)
+        {
+            var retrieveOptions = new PlayerRetrieveOptions(
+                ChannelBehavior: connectToVoiceChannel ? PlayerChannelBehavior.Join : PlayerChannelBehavior.None);
+
+            var result = await _audioService.Players
+                .RetrieveAsync(Context, playerFactory: PlayerFactory.Vote, retrieveOptions)
+                .ConfigureAwait(false);
+
+            if (!result.IsSuccess)
+            {
+                var errorMessage = result.Status switch
+                {
+                    PlayerRetrieveStatus.UserNotInVoiceChannel => "You are not connected to a voice channel.",
+                    PlayerRetrieveStatus.BotNotConnected => "The bot is currently not connected.",
+                    _ => "Unknown error.",
+                };
+
+                await FollowupAsync(errorMessage).ConfigureAwait(false);
+                return null;
+            }
+
+            return result.Player;
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,68 +1,20 @@
-﻿using DiscordBot.Commands;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.EventArgs;
-using DSharpPlus.SlashCommands;
+﻿using Discord.Interactions;
+using Discord.WebSocket;
 using Lavalink4NET.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using DiscordBot;
 
 var builder = new HostApplicationBuilder(args);
 
-// DSharpPlus
-builder.Services.AddHostedService<ApplicationHost>();
-builder.Services.AddSingleton<DiscordClient>();
-builder.Services.AddSingleton(new DiscordConfiguration { Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"), }); // Put token here
+// Discord
+builder.Services.AddSingleton<DiscordSocketClient>();
+builder.Services.AddSingleton<InteractionService>();
+builder.Services.AddHostedService<DiscordClientHost>();
 
+// Lavalink
 builder.Services.AddLavalink();
-
-// Logging
-builder.Services.AddLogging(s => s.AddConsole().SetMinimumLevel(LogLevel.Trace));
+builder.Services.AddLogging(x => x.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
 builder.Build().Run();
-
-file sealed class ApplicationHost : BackgroundService
-{
-    private readonly IServiceProvider _serviceProvider;
-    private readonly DiscordClient _discordClient;
-
-    public ApplicationHost(IServiceProvider serviceProvider, DiscordClient discordClient)
-    {
-        ArgumentNullException.ThrowIfNull(serviceProvider);
-        ArgumentNullException.ThrowIfNull(discordClient);
-
-        _serviceProvider = serviceProvider;
-        _discordClient = discordClient;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        _discordClient
-            .UseSlashCommands(new SlashCommandsConfiguration { Services = _serviceProvider })
-            .RegisterCommands<MusicCommands>(); // Add guild id here
-
-        // connect to discord gateway and initialize node connection
-        await _discordClient
-            .ConnectAsync()
-            .ConfigureAwait(false);
-
-        var readyTaskCompletionSource = new TaskCompletionSource();
-
-        Task SetResult(DiscordClient client, ReadyEventArgs eventArgs)
-        {
-            readyTaskCompletionSource.TrySetResult();
-            return Task.CompletedTask;
-        }
-
-        _discordClient.Ready += SetResult;
-        await readyTaskCompletionSource.Task.ConfigureAwait(false);
-        _discordClient.Ready -= SetResult;
-
-        await Task
-            .Delay(Timeout.InfiniteTimeSpan, stoppingToken)
-            .ConfigureAwait(false);
-    }
-}
