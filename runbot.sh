95a2f70067c40341305d0881c9b2f816f0e5d6aa
xrhr <xxrhr@yandex.ru>
2024-08-23 13:57:38 +0800

    sdfs

--- runbot.sh
+++ runbot.sh
@@ -5,7 +5,7 @@ echo ===$ docker rmi discord-bot
 docker rmi discord-bot
 
 echo ===$ docker build -t discord-bot -f ./DiscordBot/Dockerfile .
-docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
+docker build -t discord-bot -f ./DiscordBot/Dockerfile .
 
 echo ===$ docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN LAVALINK_HOST_URL=$LAVALINK_HOST_URL LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot
 docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN -e LAVALINK_HOST_URL=$LAVALINK_HOST_URL -e LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot
