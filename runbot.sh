fcaeadab9258bfea539ee9cddb06cc71b3cc93eb
xrhr <xxrhr@yandex.ru>
2024-08-23 08:34:53 +0800

    fsd

--- runbot.sh
+++ runbot.sh
@@ -1,4 +1,8 @@
+echo ===$ git pull
 git pull
+echo ===$ docker rmi discord-bot:0.1
 docker rmi discord-bot:0.1
+echo ===$ docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
 docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
+echo ===$ docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN discord-bot:0.1
 docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN discord-bot:0.1
