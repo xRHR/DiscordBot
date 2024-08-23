499f2fd6b193409f1682d0431568e0fa0e8739ac
xrhr <xxrhr@yandex.ru>
2024-08-23 13:55:03 +0800

    das

--- runbot.sh
+++ runbot.sh
@@ -1,8 +1,11 @@
 echo ===$ git pull
 git pull
-echo ===$ docker rmi discord-bot:0.1
-docker rmi discord-bot:0.1
-echo ===$ docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
+
+echo ===$ docker rmi discord-bot
+docker rmi discord-bot
+
+echo ===$ docker build -t discord-bot -f ./DiscordBot/Dockerfile .
 docker build -t discord-bot:0.1 -f ./DiscordBot/Dockerfile .
-echo ===$ docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN LAVALINK_HOST_URL=$LAVALINK_HOST_URL LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot:0.1
-docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN LAVALINK_HOST_URL=$LAVALINK_HOST_URL LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot:0.1
+
+echo ===$ docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN LAVALINK_HOST_URL=$LAVALINK_HOST_URL LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot
+docker run --rm --name discord-bot -e DISCORD_TOKEN=$DISCORD_TOKEN -e LAVALINK_HOST_URL=$LAVALINK_HOST_URL -e LAVALINK_HOST_PASSWORD=$LAVALINK_HOST_PASSWORD discord-bot
